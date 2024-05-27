52960c968ede37558ca85b7829c400ca8c070682
Abiria <abiria.dev@gmail.com>
2024-05-27 21:27:21 +0900

    chore: add primary palette

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,22 @@ export default {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					50: '#ecfdf7',
+					100: '#d1fae9',
+					200: '#a8f2d7',
+					300: '#6fe6c2',
+					400: '#36d1a7',
+					500: '#12b890',
+					600: '#079475',
+					700: '#057760',
+					800: '#075e4e',
+					900: '#074d41',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
