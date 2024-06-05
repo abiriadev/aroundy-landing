90ae6260e8259e17797993faf0691e9eeba105c6
Abiria <abiria.dev@gmail.com>
2024-06-05 19:30:49 +0900

    fix: setup vercel static imageservice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), icon()],
+	output: 'static',
+	adapter: vercel({
+		imageService: true,
+	}),
 })
